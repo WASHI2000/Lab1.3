9782ec74f35ce4ebf77a29040ef6816e374e1e5c
Mr-polo11polo11 <101180608+WASHI2000@users.noreply.github.com>
2022-03-22 10:25:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    double pierwsza = 2;
+    double druga = 3;
+     double potega = Math.pow( pierwsza, druga);
+    System.out.println("Liczba " + pierwsza + " podniesiona do potegi " + druga + " to " +potega );
+  }
+}
